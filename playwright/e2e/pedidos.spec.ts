6460108c28d0b9a7de1a6b8b991c27e4825feeaf
felipedanielsousa <felipedanielsousa@gmail.com>
2026-03-02 11:53:45 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,30 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto(' http://localhost:5173/');
-  //checkpoint
+  
+ // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  //acessa página de consulta
+ 
+ // Act
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  //checkpoint
+
+ // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-OREROJ');
 
+  // Act
   await page.getByTestId('search-order-button').click();
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-OREROJ');
 
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
-
 
 });
